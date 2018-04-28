5a6f70e492ce655742802de26922d6b350b5e212
Gregory Higley <code@revolucent.net>
2018-04-28 09:16:45 -0400

    AbsoluteDate

---
+++ AbsoluteDate/AbsoluteDate.h
@@ -0,0 +1,19 @@
+//
+//  AbsoluteDate.h
+//  AbsoluteDate
+//
+//  Created by Gregory Higley on 4/27/18.
+//  Copyright © 2018 Gregory Higley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AbsoluteDate.
+FOUNDATION_EXPORT double AbsoluteDateVersionNumber;
+
+//! Project version string for AbsoluteDate.
+FOUNDATION_EXPORT const unsigned char AbsoluteDateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbsoluteDate/PublicHeader.h>
+
+
